db7c5c6261d51f4af95e0e90349c97798069b54f
filipenkaProg <maksim.filipenka@inputhealth.com>
2022-02-23 17:29:40 +0300

    authorization configured

--- gruzmaster_back/app/controllers/users/sessions_controller.rb
+++ gruzmaster_back/app/controllers/users/sessions_controller.rb
@@ -4,7 +4,7 @@ class Users::SessionsController < Devise::SessionsController
   private
 
   def respond_with(resource, _opts = {})
-    render json: { message: 'You are logged in.' }, status: :ok
+    render json: { token: current_token, user_id: current_user.id, message: 'You are logged in.' }, status: :ok
   end
 
   def respond_to_on_destroy
@@ -19,5 +19,9 @@ class Users::SessionsController < Devise::SessionsController
 
   def log_out_failure
     render json: { message: "Hmm nothing happened."}, status: :unauthorized
+  end
+
+  def current_token
+    request.env['warden-jwt_auth.token']
   end
 end
--- gruzmaster_back/config/routes.rb
+++ gruzmaster_back/config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users,
   controllers: {
-      sessions: 'users/sessions',
-      registrations: 'users/registrations'
+      sessions: 'users/sessions', # curl -XPOST -i -H "Content-Type: application/json" -d '{ "user": { "email": "test@example.com", "password": "12345678" } }' http://localhost:3000/users/sign_in
+      registrations: 'users/registrations' # curl -XPOST -H "Content-Type: application/json" -d '{ "user": { "email": "test@example.com", "password": "12345678" } }' http://localhost:3000/users
   }
   get '/member-data', to: 'members#show'
 end
