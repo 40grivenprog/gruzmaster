42d3ea312b1a52b265bc0642b0fd551034f867f8
filipenkaProg <maksim.filipenka@inputhealth.com>
2022-03-15 22:55:17 +0300

    update signup message

--- gruzmaster_back/app/controllers/users/registrations_controller.rb
+++ gruzmaster_back/app/controllers/users/registrations_controller.rb
@@ -1,6 +1,18 @@
 class Users::RegistrationsController < Devise::RegistrationsController
   respond_to :json
 
+  def create
+    begin
+      super
+    rescue Exception => e
+      if e.is_a? ActiveRecord::RecordNotUnique
+        register_failed("This email already exists")
+      else
+        register_failed(message)
+      end
+    end
+  end
+
   private
 
   def respond_with(resource, _opts = {})
@@ -13,7 +25,7 @@ class Users::RegistrationsController < Devise::RegistrationsController
     render json: { message: 'Signed up sucessfully.' }
   end
 
-  def register_failed
-    render json: { message: "Something went wrong." }
+  def register_failed(message="Something went wrong.")
+    render json: { message: message }, status: 500
   end
 end
